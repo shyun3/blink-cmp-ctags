b1f008fdb34cfe0543c6c6d490960dafeccc8d4f
Simon Ernst <hello@simonernst.com>
2024-10-10 15:18:12 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# blink-cmp-ctags
+
+Adds support for ctags to [Saghen/blink.cmp](https://github.com/Saghen/blink.cmp).
+
+## Installation
+Add your tag files to vim
+`vim.opt.tags:append(".git/tags", "tags")`
+
+Add provider in `lazy.nvim`
+```lua
+{
+    "saghen/blink.cmp",
+    dependencies = {
+        "netmute/blink-cmp-ctags",
+    },
+    opts = {
+        sources = {
+            providers = {
+                {
+                    { "blink-cmp-ctags" },
+                    { "blink.cmp.sources.buffer" },
+                    { "blink.cmp.sources.path" },
+                },
+            },
+        },
+    },
+},
+```
---
+++ lua/blink-cmp-ctags.lua
@@ -0,0 +1,42 @@
+local function get_current_word_prefix()
+	local cursor = vim.api.nvim_win_get_cursor(0)
+	local row = cursor[1] - 1
+	local col = cursor[2]
+	local line = vim.api.nvim_get_current_line()
+	local prefix = line:sub(1, col):match("[%w_]*$") or ""
+	return prefix
+end
+
+local function tags_to_items(tags)
+	local items = {}
+	for _, tagname in ipairs(tags) do
+		table.insert(items, {
+			label = tagname,
+			kind = vim.lsp.protocol.CompletionItemKind.Field,
+			insertText = tagname,
+		})
+	end
+	return items
+end
+
+local ctags = {}
+
+function ctags.new()
+	return setmetatable({}, { __index = ctags })
+end
+
+function ctags:get_completions(_, callback)
+	local prefix = get_current_word_prefix()
+	local tags = vim.fn.getcompletion(prefix .. "*", "tag")
+	local items = tags_to_items(tags)
+
+	callback({
+		is_incomplete_forward = false,
+		is_incomplete_backward = false,
+		items = items,
+	})
+
+	return function() end
+end
+
+return ctags
