1f87e0d0688914f495b34d493203133b94d42f7f
Simon Ernst <hello@simonernst.com>
2024-10-14 07:52:46 +0200

    Bail if there are no tags files

--- lua/blink-cmp-ctags.lua
+++ lua/blink-cmp-ctags.lua
@@ -1,6 +1,5 @@
 local function get_current_word_prefix()
 	local cursor = vim.api.nvim_win_get_cursor(0)
-	local row = cursor[1] - 1
 	local col = cursor[2]
 	local line = vim.api.nvim_get_current_line()
 	local prefix = line:sub(1, col):match("[%w_]*$") or ""
@@ -26,6 +25,16 @@ function ctags.new()
 end
 
 function ctags:get_completions(_, callback)
+	-- Check if there are any tags files
+	if #vim.fn.tagfiles() == 0 then
+		callback({
+			is_incomplete_forward = false,
+			is_incomplete_backward = false,
+			items = {},
+		})
+		return function() end
+	end
+
 	local prefix = get_current_word_prefix()
 	local tags = vim.fn.getcompletion(prefix .. "*", "tag")
 	local items = tags_to_items(tags)
